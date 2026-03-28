775aaffed89c9f8e6fc2c327552074ed6fb20a24
Роман <zhanroman2610@gmail.com>
2026-03-28 16:28:37 +0500

    chore: add gh-pages deploy script, remove GH Actions, set base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
   root: 'src',
+  base: process.env.NODE_ENV === 'production' ? '/smartroom-storage/' : '/',
   server: {
     port: 3000,
     open: true
